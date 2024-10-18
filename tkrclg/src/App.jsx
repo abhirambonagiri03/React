3a48e3e6b2b55fc46c79b57f0a7dc6613fad0b64
bonagiriabhiram03 <bonagiriabhiram8@gmail.com>
2024-10-18 14:33:19 +0530

    first commit

---
+++ tkrclg/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+  <>
+   <h1>Hello world!</h1>
+   <input type="text" placeholder="enter name"/>
+    <input type="email" placeholder="enter email"/>
+    <input type="date" placeholder="enter age"/>
+    <input type="button" value="submit"/>
+  </> 
+  )
+}
+
+export default App
